7b2e61ef7aff2f0a4e6738c99734f90d1e3c12ec
mrksss8 <devmrksss@gmail.com>
2022-01-16 15:14:46 +0800

    remove officials sedeer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ResidenceSeeder::class);
+        $this->call(OfficialsSeeder::class);
         $this->call(UserSeeder::class);
      
     }
